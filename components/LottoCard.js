fa0067b0ca9b974359bc7d1165816082eb81b92c
SnoopyKim <snoopykim0@gmail.com>
2020-12-30 21:59:05 +0900

    LottoCard 재구성
    child component 추가
    
    애니메이션 추가

--- components/LottoCard.js
+++ components/LottoCard.js
@@ -1,32 +1,99 @@
-import React, { useState } from 'react';
-import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
+import { Animated, Image, StyleSheet, Text, TouchableOpacity, View, Easing } from 'react-native';
 import iconArrow from '../images/chevron-down.png';
 import { getCurrentRound } from '../modules/api';
 import Ball from './Ball';
+import { THEME_COLORS } from './../style/color';
 
-export default function LottoCard({ data }) {
+export default function LottoCard({ children, data }) {
     // 최신 결과만 처음부터 열려있는 상태
-    const [isOpen, setIsOpen] = useState(data.round === getCurrentRound());
+    const [opened, setOpened] = useState(false);
+    const [mounted, setMounted] = useState(false);
+    const [cardHeight, setCardHeight] = useState(0);
+    const [contentHeight, setContentHeight] = useState(0);
 
-    const toggleCard = () => {
-        setIsOpen(!isOpen);
+    const rotateValue = useRef(new Animated.Value(0)).current;
+    const rotate = rotateValue.interpolate({
+        inputRange: [0, 1],
+        outputRange: ['0deg', '180deg'],
+    });
+    const transValue = useRef(new Animated.Value(0)).current;
+    const heightValue = useRef(new Animated.Value(0)).current;
+
+    const onCardMounted = (e) => {
+        if (!mounted) {
+            setCardHeight(e.nativeEvent.layout.height);
+        }
+    };
+    const onContentMounted = (e) => {
+        if (!mounted) {
+            setContentHeight(e.nativeEvent.layout.height);
+        }
     };
 
+    useEffect(() => {
+        if (cardHeight * contentHeight !== 0) {
+            heightValue.setValue(cardHeight);
+            setMounted(true);
+        }
+    }, [cardHeight, contentHeight]);
+
+    useEffect(() => {
+        if (mounted) {
+            setOpened(data.round === getCurrentRound());
+        }
+    }, [mounted]);
+
+    useEffect(() => {
+        if (!mounted) {
+            return;
+        }
+        const rotateAnimation = Animated.timing(rotateValue, {
+            toValue: opened ? 1 : 0,
+            duration: 300,
+            easing: Easing.linear,
+            useNativeDriver: true,
+        });
+        const heightAnimation = Animated.timing(heightValue, {
+            toValue: opened ? cardHeight + contentHeight : cardHeight,
+            duration: 300,
+            easing: Easing.linear,
+            useNativeDriver: false,
+        });
+        const transAnimation = Animated.timing(transValue, {
+            toValue: opened ? contentHeight : 0,
+            duration: 300,
+            easing: Easing.linear,
+            useNativeDriver: true,
+        });
+        Animated.parallel([rotateAnimation, heightAnimation, transAnimation]).start();
+    }, [opened]);
+
     return (
-        <View style={styles.container}>
-            <TouchableOpacity style={styles.header} onPress={toggleCard}>
+        <Animated.View
+            style={[styles.container, { height: mounted ? heightValue : undefined }]}
+            onLayout={onCardMounted}>
+            <TouchableOpacity
+                style={styles.header}
+                activeOpacity={1.0}
+                onPress={() => setOpened(!opened)}>
                 <View style={styles.headerTitle}>
                     <Text style={styles.title}>{data.round}회</Text>
                     <Text style={styles.subtitle}>{data.date}</Text>
                 </View>
-                <Image
-                    style={[styles.icon, isOpen && styles.reverse]}
+                <Animated.Image
+                    style={[styles.icon, { transform: [{ rotate: rotate }] }]}
                     source={iconArrow}
                     resizeMode={'contain'}
                 />
             </TouchableOpacity>
-            {isOpen && (
-                <View style={styles.content}>
+            <Animated.View
+                style={[
+                    styles.content,
+                    { marginTop: -75, transform: [{ translateY: transValue }] },
+                ]}
+                onLayout={onContentMounted}>
+                <View style={styles.numberView}>
                     <Ball number={data.no1} style={styles.firstBall} />
                     <Ball number={data.no2} style={styles.ball} />
                     <Ball number={data.no3} style={styles.ball} />
@@ -36,36 +103,45 @@ export default function LottoCard({ data }) {
                     <Text style={styles.dividing}>+</Text>
                     <Ball number={data.bno} style={styles.bonusBall} />
                 </View>
-            )}
-        </View>
+                {children}
+            </Animated.View>
+        </Animated.View>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
-        borderWidth: 1,
-        borderColor: 'black',
-        paddingHorizontal: 20,
-        paddingVertical: 10,
         marginVertical: 5,
-        backgroundColor: 'white',
+        minHeight: 60,
+        backgroundColor: THEME_COLORS.GRAY_50,
+        borderRadius: 20,
+        overflow: 'hidden',
+        elevation: 2,
+        margin: 2,
     },
     header: {
+        backgroundColor: THEME_COLORS.GRAY_50,
+        paddingHorizontal: 20,
+        paddingVertical: 10,
+        borderRadius: 20,
         flexDirection: 'row',
-        height: 40,
+        zIndex: 5,
     },
     headerTitle: {
         flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center',
     },
     title: {
         color: 'black',
         fontSize: 16,
-        fontWeight: '500',
+        fontWeight: '700',
     },
     subtitle: {
         color: 'gray',
         fontSize: 13,
         fontWeight: '400',
+        marginStart: 10,
     },
     icon: {
         marginVertical: 5,
@@ -77,23 +153,20 @@ const styles = StyleSheet.create({
     },
     content: {
         flex: 1,
+        borderRadius: 20,
+        paddingHorizontal: 10,
+        paddingTop: 5,
+        paddingBottom: 15,
+        backgroundColor: THEME_COLORS.GRAY_50,
+    },
+    numberView: {
         flexDirection: 'row',
-        paddingVertical: 10,
-    },
-    ball: {
-        marginHorizontal: 3,
-    },
-    firstBall: {
-        marginEnd: 3,
-    },
-    bonusBall: {
-        marginStart: 3,
+        justifyContent: 'space-around',
     },
     dividing: {
         color: 'black',
         lineHeight: 36,
         fontSize: 24,
         fontWeight: '700',
-        marginHorizontal: 3,
     },
 });
--- routes/HistoryTabView.js
+++ routes/HistoryTabView.js
@@ -1,13 +1,15 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, TouchableOpacity } from 'react-native';
 import { isEmptyArray } from './../utils/check';
 import { getAllLottoDatas } from './../modules/api';
 import LottoCard from './../components/LottoCard';
 import { setLoading } from '../components/Popup';
 import { setAlert } from './../components/Popup/index';
+import { THEME_COLORS } from './../style/color';
 
 export default function HistoryTabView() {
     const [data, setData] = useState([]);
+    const [selected, setSelected] = useState({});
 
     useEffect(() => {
         if (isEmptyArray(data)) {
@@ -17,22 +19,57 @@ export default function HistoryTabView() {
                 setLoading(false);
                 setAlert(true, '데이터 불러오기 완료');
             });
+        } else {
+            setSelected(data[0]);
         }
     }, [data]);
 
+    useEffect(() => {
+        console.log(selected);
+    }, [selected]);
+
     return (
-        <View>
-            {isEmptyArray(data) ? (
-                <Text style={{ alignSelf: 'center' }}>Data Loading...</Text>
-            ) : (
-                <ScrollView>
+        <View style={styles.screen}>
+            <View>
+                <Text style={{ color: THEME_COLORS.GRAY_100 }}>{JSON.stringify(selected)}</Text>
+            </View>
+            <View style={styles.listContainer}>
+                <ScrollView style={styles.scrollView}>
                     {data.map((val) => (
-                        <LottoCard key={val.round} data={val} />
+                        <LottoCard key={val.round} data={val}>
+                            <TouchableOpacity style={styles.link} onPress={() => setSelected(val)}>
+                                <Text style={styles.textLink}>당첨금 확인하기</Text>
+                            </TouchableOpacity>
+                        </LottoCard>
                     ))}
                 </ScrollView>
-            )}
+            </View>
         </View>
     );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        backgroundColor: THEME_COLORS.MIDNIGHT_BLACK,
+    },
+    listContainer: {
+        flex: 1,
+        paddingHorizontal: 20,
+        paddingTop: 10,
+        backgroundColor: THEME_COLORS.GRAY_100,
+        borderTopStartRadius: 30,
+        borderTopEndRadius: 30,
+    },
+    scrollView: {
+        paddingVertical: 10,
+    },
+    link: {
+        alignSelf: 'flex-end',
+    },
+    textLink: {
+        fontSize: 14,
+        textDecorationLine: 'underline',
+        color: THEME_COLORS.GRAY_500,
+    },
+});
